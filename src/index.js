8fe229c223ca64f6c6c93557e1a4d00cab54a2cc
Ive Liana <ivefransisca13@gmail.com>
2021-03-04 13:20:07 +0700

    add css, postcss, sass-loader, hostreloading

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles/index.scss";
+
 const testingRecipe ={
 	leat: 1,
 	testl: 3,
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 let mode = "development"
+let target = "web"
 if(process.env.NODE_ENV === "production"){
 	mode= "production";
+	target="browserslist"
 }
 
 module.exports = {
 	mode: mode,
+	target: target,
 	module: {
 		rules: [
+			{
+				test: /\.(s[ac]|c)ss$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
@@ -18,6 +27,8 @@ module.exports = {
 	},
 	devtool: "source-map",
 	devServer: {
-		contentBase: "./dist"
-	}
+		contentBase: "./dist",
+		hot: true
+	}, 
+	plugins: [ new MiniCssExtractPlugin()]
 }
